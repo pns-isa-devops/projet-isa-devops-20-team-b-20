f7eca374a6bcce23a1c73f3ef004ac3eb0945213
Alexandre Lon <alexandre.longordo@etu.univ-cotedazur.fr>
2020-03-19 09:47:18 +0100

    Update run.sh script

--- run.sh
+++ run.sh
@@ -1,16 +1,20 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 if [ $# -eq 0 ]
 then
   echo "No project selected"
+  echo "$0 help : display help"
   exit
 fi
 
 if [ $1 = "help" ]
 then
-  echo "Help command"
+  echo "----- Help command -----"
+  echo "help : Print this help"
   echo "all : Compile all project"
-  
+  echo "clean : Removing all executable"
+  echo "mr-proper : Removing all configurable files"
+
   echo "cli-office : Compile clien office project"
   echo "    clean : Clean project"
   echo "    run : Run project"
@@ -27,10 +31,35 @@ then
   echo "    run : Run project"
 
   echo "examples to use"
-  echo "  ./run cli-office clean run"
-  echo "  ./run cli-office clean"
-  echo "  ./run cli-office run"
-  echo "  ./run cli-office"
+  echo "  $0 cli-office clean run"
+  echo "  $0 cli-office clean"
+  echo "  $0 cli-office run"
+  echo "  $0 cli-office"
+  exit
+fi
+
+if [ $1 = "clear" ]
+then
+  rm -rf projet-isa-devops-20-team-b-20-client-office/target
+  rm -rf projet-isa-devops-20-team-b-20-client-warehouse/target
+  rm -rf projet-isa-devops-20-team-b-20-drone-delivery/target
+  rm -rf projet-isa-devops-20-team-b-20-drone-api/server.exe
+  exit
+fi
+
+if [ $1 = "mr-proper" ]
+then
+  rm -rf projet-isa-devops-20-team-b-20-client-office/target
+  rm -rf projet-isa-devops-20-team-b-20-client-warehouse/target
+  rm -rf projet-isa-devops-20-team-b-20-drone-delivery/target
+  rm -rf projet-isa-devops-20-team-b-20-drone-api/server.exe
+  rm -rf .vscode/
+  rm -rf .idea/
+  rm -rf projet-isa-devops-20-team-b-20-client-office/*.iml
+  rm -rf projet-isa-devops-20-team-b-20-client-warehouse/*.iml
+  rm -rf projet-isa-devops-20-team-b-20-drone-delivery/*/*.iml
+  rm -rf projet-isa-devops-20-team-b-20-drone-delivery/*.iml
+  exit
 fi
 
 if [ $1 = "all" ]
@@ -45,8 +74,9 @@ then
   mvn clean package
   cd ..
   cd projet-isa-devops-20-team-b-20-drone-api || exit
-   ./compile.sh
-   echo "All projects compiled"
+  ./compile.sh
+  echo "All projects compiled"
+  exit
 fi
 
 if [ $1 = "cli-office" ]
