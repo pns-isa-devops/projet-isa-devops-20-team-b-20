efb4e928177117823dfd55ab404c44c349aca9af
AlexisLefebvre1953 <alexis.lefebvre@etu.univ-cotedazur.fr>
2020-04-03 16:44:19 +0200

    #43 updated docker files for api (names)

--- docker/api-drone/build.sh
+++ docker/api-drone/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #Preparing environment
-cd ../../dotNet
+cd ../../projet-isa-devops-20-team-b-20-drone-api
 ./compile.sh
-cp ./server.exe ../docker/partners/.
+cp ./server.exe ../docker/api-drone/.
 
 # building the docker image
-cd ../docker/partners/
-docker build -t petitroll/tcf-ext .
+cd ../docker/api-drone/
+docker build -t dd-api-drone .
 
 # cleaning up the environment
 rm -rf server.exe
--- docker/api-drone/publish.sh
+++ docker/api-drone/publish.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker push petitroll/tcf-ext
+docker push dd-api-drone
