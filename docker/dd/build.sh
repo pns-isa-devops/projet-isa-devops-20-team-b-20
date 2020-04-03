4abc86a03c5655015b391639b2c3fa9f2a9a3dad
AlexisLefebvre1953 <alexis.lefebvre@etu.univ-cotedazur.fr>
2020-04-03 11:43:47 +0200

    #40 error on paths corrected

--- docker/dd/build.sh
+++ docker/dd/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #Preparing environment
-cd ../..
+cd ../../projet-isa-devops-20-team-b-20-drone-delivery
 echo "Compiling the DD system"
 mvn -q -DskipTests clean package
 echo "Done"
-cd docker/dd
+cd ../docker/dd
 
-cp ../../projet-isa-devops-20-team-b-20-web-service/target/drone-delivery-backend.war .
+cp ../../projet-isa-devops-20-team-b-20-drone-delivery/projet-isa-devops-20-team-b-20-web-service/target/drone-delivery-backend.war .
 
 # building the docker image
 docker build -t drone-delivery .
