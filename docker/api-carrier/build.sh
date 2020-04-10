7253d6ca4bac77a893f487234718176ec277668d
AlexisLefebvre1953 <alexis.lefebvre@etu.univ-cotedazur.fr>
2020-04-10 10:50:23 +0200

    pns-isa-devops/projet-isa-devops-20-team-b-20#62 docker file for carrier api and script of docker compose modified in order to include this new API

---
+++ docker/api-carrier/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Preparing environment
+cd ../../projet-isa-devops-20-team-b-20-carrier-api
+./compile.sh
+cp ./server.exe ../docker/api-carrier/.
+
+# building the docker image
+cd ../docker/api-carrier/
+docker build -t dd-api-carrier .
+
+# cleaning up the environment
+rm -rf server.exe
